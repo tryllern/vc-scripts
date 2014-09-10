ca145a22d32c672e51a3de06c517ac4b843d192b
Daniel Nilsen <daniel@narver.net>
2014-09-10 11:30:18 +0200

    fixed error in performance output

--- nagios-scripts/nagios_check_acano.py
+++ nagios-scripts/nagios_check_acano.py
@@ -53,7 +53,7 @@ else:
 		warning=int(options.critical)
 	else:
 		warning=int(options.warning)
-			
+
 	critical=int(options.critical)
 
 	if not options.max:
@@ -72,9 +72,9 @@ else:
 
 
 	if active_ports==0:
-		print "utilization:0% |active_ports:0:{}:{}".format(warning,critical)
+		print "utilization:0% |active_ports=0:{}:{}".format(warning,critical)
 	else:
 		percent_active_ports=int((float(active_ports)/float(max_calls))*100.0)
-		print "utilization:{}% |active_ports:{};{};{}".format(percent_active_ports,active_ports,warning,critical)
+		print "utilization:{}% |active_ports={};{};{}".format(percent_active_ports,active_ports,warning,critical)
 
 	exit(exit_code)

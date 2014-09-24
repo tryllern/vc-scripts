3c119e31c74f0bd493e4c1f48fbdde15a4150df1
Daniel Nilsen <daniel@narver.net>
2014-09-24 16:44:05 +0200

    now supports both mxp and c codecs, autodetects plattform

--- nagios-scripts/nagios_check_endpoint.py
+++ nagios-scripts/nagios_check_endpoint.py
@@ -23,64 +23,174 @@ sip_code=0
 exit_text="Connection Error"
 
 
-sip_response=requests.get(
-    	"https://"+options.ip+'/getxml?location=/Status/SIP/Profile/Registration/Status',
-    	auth=(options.username, options.password),
-    	verify=False,)
 
-if sip_response.status_code==200:
+def getline(ip,username,password,url,identifier):
+
+	'''
+	ip= address to codec
+	username= username to codec
+	passord= passowrd to codec
+	url= the part after getxml?location=' to extract from codec
+	identifier= the regex search-string
+	'''
+	response=requests.get(
+    	"https://"+ip+'/getxml?location='+url,
+    	auth=(username, password),
+    	verify=False,)
+	if response.status_code==200:
+		regex=re.compile(identifier)
+		status=regex.search(response.text)
+		if status:
+			return status.group(1)
+		else:
+			return None
+	else:
+		return None
+
+
+
+
+software_version=getline(ip=options.ip,
+					  username=options.username,
+					  password=options.password,
+					  url="/Status/SystemUnit/Software/Version",
+					  identifier='<Version item=\"1\">(.+)</Version>'
+					  )
+
+if software_version:
+	if software_version.lower().startswith("tc"):
 		
-	regex =re.compile("<Status item=\"1\">(.+)</Status>")
-	status=regex.search(sip_response.text)
-	if status:
-		status=status.group(1)
-		if status=="Inactive":
-			sip_code=1
-			exit_text="SIP-Status:{}".format(status)
 
-		elif status=="Registered":
-			sip_code=0
-			exit_text="SIP-Status:{}".format(status)
+			sip_response=getline(ip=options.ip,
+								  username=options.username,
+								  password=options.password,
+								  url="/Status/SIP/Profile/Registration/Status",
+								  identifier='<Status item=\"1\">(.+)</Status>'
+								  )
 
-		elif status=="Rejected":
-			sip_code=2
-			exit_text="SIP-Status:{}".format(status)			
 
-		else:
-			sip_code=3
-			exit_text="SIP-Status:{}".format(status)
-			
-			
-else:
-	sip_code=3
+			if sip_response:		
+				
+				
+					status=sip_response.lower()
+					if status=="inactive":
+						sip_code=1
+						exit_text="SIP-Status:{}".format(status)
 
-h323_response=requests.get(
-    	"https://"+options.ip+'/getxml?location=/Status/H323/Gatekeeper/Status',
-    	auth=(options.username, options.password),
-    	verify=False,)
+					elif status=="registered":
+						sip_code=0
+						exit_text="SIP-Status:{}".format(status)
 
-if h323_response.status_code==200:
-	regex=re.compile("<Status item=\"1\">(.+)</Status>")
-	status=regex.search(h323_response.text)
-	if status:
-		status=status.group(1)
-		if status=="Inactive":
-			h323_code=1
-			exit_text +=" H323-Status:{}".format(status)
+					elif status=="rejected":
+						sip_code=2
+						exit_text="SIP-Status:{}".format(status)			
 
-		elif status=="Registred":
-			h323_code=0
-			exit_text +=" H323-Status:{}".format(status)
+					else:
+						sip_code=3
+						exit_text="SIP-Status:{}".format(status)
+						
+						
+			else:
+				sip_code=3
 
-		elif status=="Rejected":
-			h323_code=2
-			exit_text +=" H323-Status:{}".format(status)	
 
-		else:
-			h323_code=3
-			exit_text +=" H323-Status:{}".format(status)
-else:
-	h323_code=3
+
+			h323_response=getline(ip=options.ip,
+								  username=options.username,
+								  password=options.password,
+								  url="/Status/H323/Gatekeeper/Status",
+								  identifier='<Status item=\"1\">(.+)</Status>'
+								  )
+
+			if h323_response:
+
+				
+					status=h323_response.lower()
+					if status=="inactive":
+						h323_code=1
+						exit_text +=" H323-Status:{}".format(status)
+
+					elif status=="registered":
+						h323_code=0
+						exit_text +=" H323-Status:{}".format(status)
+
+					elif status=="rejected":
+						h323_code=2
+						exit_text +=" H323-Status:{}".format(status)	
+
+					else:
+						h323_code=3
+						exit_text +=" H323-Status:{}".format(status)
+			else:
+				
+				h323_code=3
+
+
+	if software_version.lower().startswith("f"):
+
+			sip_response=getline(ip=options.ip,
+					  username=options.username,
+					  password=options.password,
+					  url="/Status/SIP/Registration",
+					  identifier='<Registration item="1" status="(.+)">'
+					  )
+
+
+			if sip_response:		
+				
+				
+					status=sip_response.lower()
+					if status=="inactive":
+						sip_code=1
+						exit_text="SIP-Status:{}".format(status)
+
+					elif status=="registered":
+						sip_code=0
+						exit_text="SIP-Status:{}".format(status)
+
+					elif status=="rejected":
+						sip_code=2
+						exit_text="SIP-Status:{}".format(status)			
+
+					else:
+						sip_code=3
+						exit_text="SIP-Status:{}".format(status)
+						
+						
+			else:
+				sip_code=3
+
+
+
+			h323_response=getline(ip=options.ip,
+					  username=options.username,
+					  password=options.password,
+					  url="/Status/H323GateKeeper",
+					  identifier='<H323Gatekeeper item="1" status="(.+)">'
+					  )
+
+			if h323_response:
+
+				
+					status=h323_response.lower()
+					if status=="inactive":
+						h323_code=1
+						exit_text +=" H323-Status:{}".format(status)
+
+					elif status=="registered":
+						h323_code=0
+						exit_text +=" H323-Status:{}".format(status)
+
+					elif status=="rejected":
+						h323_code=2
+						exit_text +=" H323-Status:{}".format(status)	
+
+					else:
+						h323_code=3
+						exit_text +=" H323-Status:{}".format(status)
+			else:
+				
+				h323_code=3
 
 #exit code 0
 if sip_code==0 and h323_code==0:
@@ -122,5 +232,8 @@ elif sip_code==3 and h323_code==1:
 
 
 # print status and exit
+print h323_code
+print sip_code
+print exit_code
 print exit_text
 exit(exit_code)
